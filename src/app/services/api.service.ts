32ea67a75140d9e5d19eaeda0b4a2f0966dff3a8
PaterDev <josepater_1130@hotmail.com>
2024-01-25 13:20:49 -0500

    chore: services http to api products

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  private _http = inject(HttpClient);
+  private urlBase: string = 'https://fakestoreapi.com/products';
 
-  constructor() { }
+  getProducts(): Observable<any> {
+    return this._http.get(this.urlBase);
+  }
+
+  getProductId(id: number): Observable<any> {
+    return this._http.get(`${this.urlBase}/${id}`);
+  }
 }
