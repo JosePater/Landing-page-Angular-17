c95517fb78da9ad8d3d254b4f7b442dff6d1a36a
PaterDev <josepater_1130@hotmail.com>
2024-01-25 11:09:55 -0500

    feat: added navbar and routers

---
+++ src/app/pages/products-detail/products-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-products-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './products-detail.component.html',
+  styleUrl: './products-detail.component.css'
+})
+export class ProductsDetailComponent {
+
+}
